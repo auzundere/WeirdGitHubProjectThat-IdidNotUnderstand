48094067100225d78e471b71513b084c4b3c6f90
handan.tekin85@gmail.com <handan.tekin85@gmail.com>
2018-03-12 10:40:51 -0500

    Handan's First Change

--- src/pages/FirstClass.java
+++ src/pages/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First Message");
 		System.out.println("Second Message - Abdullah");
-
+		System.out.println("Second Message-Handan");
 	}
 
 }
