fd6c378a7eeb052850c5c7d01d7a42695d1a7818
auzundere <admin@192.168.1.2>
2018-03-12 11:06:27 -0500

    Abdullah's Second Change

--- src/pages/FirstClass.java
+++ src/pages/FirstClass.java
@@ -7,6 +7,7 @@ public class FirstClass {
 		System.out.println("Second Message - Abdullah");
 		System.out.println("Second Message-Handan");
 		System.out.println("Second Message - Fatime");
+		System.out.println("Third Message - Abdullah");//This is Abdullah's last work before stashing.
 	}
 
 }
