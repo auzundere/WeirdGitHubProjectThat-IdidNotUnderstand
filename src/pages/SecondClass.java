1a451a5e7069d0d02670dd71539cd79ccff2c7f9
fatimesimsek <fatimasimsek@gmail.com>
2018-03-12 14:17:37 -0400

    thirdclass

--- src/pages/SecondClass.java
+++ src/pages/SecondClass.java
@@ -5,7 +5,7 @@ public class SecondClass {
 	public static void main(String[] args) {
 		System.out.println("Second class/ CReated by Evgeniya");
 		System.out.println("Try one more  time"); // from Evgeniya
-
+System.out.println("How to add new class?");
 	}
 
 }
---
+++ src/pages/thirdclass.java
@@ -0,0 +1,12 @@
+package pages;
+
+public class thirdclass {
+
+	public static void main(String[] args) {
+		int i=5;
+		int b=2;
+		int sum=i+b;
+		System.out.println(sum);
+	}
+
+}
