a8b209ce70c4f4aac04879694fc874e7746a9c4b
janerra <janerra32@gmail.com>
2018-03-12 12:14:55 -0400

    Version Evgeniya

--- src/pages/FirstClass.java
+++ src/pages/FirstClass.java
@@ -8,6 +8,7 @@ public class FirstClass {
 		System.out.println("Second Message-Handan");
 		System.out.println("Second Message - Fatime");
 		System.out.println("Third Message - Abdullah");//This is Abdullah's last work before stashing.
+		System.out.println("Third Message - Evgeniya");// from Evgeniya
 	}
 
 }
