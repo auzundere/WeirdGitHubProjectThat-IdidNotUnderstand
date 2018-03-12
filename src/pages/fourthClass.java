3d74abaeb734425ebc3a091c4ae6989b61a0c861
fatimesimsek <fatimasimsek@gmail.com>
2018-03-12 14:32:36 -0400

    fourthclass

---
+++ src/pages/fourthClass.java
@@ -0,0 +1,15 @@
+package pages;
+
+public class fourthClass {
+
+	public static void main(String[] args) {
+		int[] num = new int[3];
+
+		num[0] = 5;
+		num[1] = 6;
+		num[2] = 7;
+		System.out.println(num[2]);
+
+	}
+
+}
--- src/pages/thirdclass.java
+++ src/pages/thirdclass.java
@@ -3,9 +3,9 @@ package pages;
 public class thirdclass {
 
 	public static void main(String[] args) {
-		int i=5;
+		int a=5;
 		int b=2;
-		int sum=i+b;
+		int sum=a+b;
 		System.out.println(sum);
 	}
 
